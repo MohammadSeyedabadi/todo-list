ee3cb11445e2c1cc0fb315374676a8638d1ae526
MohammadSeyedabadi <118050961+MohammadSeyedabadi@users.noreply.github.com>
2023-09-28 22:06:48 +0330

    #

--- save/src/data/projectsList.js
+++
@@ -1,8 +0,0 @@
-export const projectsList = [
-  {
-    name: 'flower-shop-2',
-    // html_url:'https://github.com/MohammadSeyedabadi/flower-shop-2',
-    demoUrl: 'https://mohammadseyedabadi.github.io/flower-shop-2/',
-    // articleUrl:'',
-  },
-]
