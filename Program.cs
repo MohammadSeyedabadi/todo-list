8626bc6e3e94727bf4089d4cc7029f848deb2bd4
MohammadSeyedabadi <118050961+MohammadSeyedabadi@users.noreply.github.com>
2023-10-15 18:03:04 +0330

    #

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace maze
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] maze =  {{1,1,0,1 },
+                            {0,1,1,1 },
+                            {0,1,0,1 },
+                            {0,1,1,1 } };
+
+            int[,] visited1 =   {{0,0,0,0 },
+                                {0,0,0,0 },
+                                {0,0,0,0 },
+                                {0,0,0,0 }};
+            visited1[0, 0] = 1;
+            RatInAMaze ratInAMaze = new RatInAMaze();
+            ratInAMaze.FindPathInMaze(maze, visited1, 0, 0, 3, 3, 1);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ RatInAMaze.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace maze
+{
+    class RatInAMaze
+    {
+        int[] Path_Row = { 0, 0, 1, -1 };
+        int[] Path_Col = { 1, -1, 0, 0 };
+
+        bool canRunInitialMove = true;
+
+        public void FindPathInMaze(int[,] maze, int[,] visited, int row, int col, int desRow, int desCol, int move)
+        {
+            if ((row == desRow) && (col == desCol))
+            {
+                for (int i = 0; i < 4; i++)
+                {
+                    for (int j = 0; j < 4; j++)
+                    {
+                        Console.Write(visited[i, j] + " ");
+                    }
+                    Console.WriteLine();
+                }
+                Console.WriteLine("================");
+
+                Console.ReadKey();
+                Environment.Exit(0);
+            }
+            else
+            {
+                for (int index = 0; index < Path_Row.Length; index++)
+                {
+                    int rowNew = row + Path_Row[index];
+                    int colNew = col + Path_Col[index];
+                    if (CanWeMove(maze, visited, rowNew, colNew))
+                    {
+                        move++;
+                        visited[rowNew, colNew] = move;
+                        drawBoard(rowNew,colNew);
+                        FindPathInMaze(maze, visited, rowNew, colNew, desRow, desCol, move);
+                      /*  move--;
+                        visited[rowNew, colNew] = 0; */
+                    }
+                }
+            }
+        }
+
+        bool CanWeMove(int[,] maze, int[,] visited, int rowNew, int colNew)
+        {
+            if ((rowNew >= 0) && (rowNew < 4) && (colNew >= 0) && (colNew < 4) && (maze[rowNew, colNew] == 1) && (visited[rowNew, colNew] == 0))
+            {
+                return true;
+            }
+            return false;
+        }
+
+
+        private void drawBoard(int rowNew, int colNew)
+        {
+            int[,] board =  {{1,1,0,1 },
+                            {0,1,1,1 },
+                            {0,1,0,1 },
+                            {0,1,1,1 } };
+
+            if (canRunInitialMove)
+            {
+                initialMove();
+                canRunInitialMove = false;
+            }
+
+            board [rowNew,colNew] = 5;
+
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    Console.Write(board[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+            Thread.Sleep(900);
+            Console.Clear();
+        }
+
+        private void initialMove()
+        {
+            Console.Write("5 1 0 1\n0 1 1 1\n0 1 0 1\n0 1 1 1");
+            Thread.Sleep(900);   
+            Console.Clear();
+        }
+    }
+}
