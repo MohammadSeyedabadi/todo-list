d736d907f869e0aa757c2e8055d01b98983b73dc
unknown <mhmd.sey.7@gmail.com>
2023-10-26 22:43:34 +0330

    #

---
+++ csharp/maza game/V3/RatInAMaze.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace maze
+{
+    class RatInAMaze
+    {
+        int[] Path_Row = { 0, 0, 1, -1 };
+        int[] Path_Col = { 1, -1, 0, 0 };
+
+        bool canRunInitialMove = true;
+        
+
+        public void FindPathInMaze(int[,] maze, int[,] visited, int row, int col, int desRow, int desCol, int move)
+        {
+            if ((row == desRow) && (col == desCol))
+            {
+                for (int i = 0; i < visited.GetLength(0); i++)
+                {
+                    for (int j = 0; j < visited.GetLength(1); j++)
+                    {
+                        if (visited[i, j] == 01 || visited[i, j] > 01) {
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.Write(visited[i, j].ToString("00") + "  ");
+                        }
+                        else if (maze[i, j] == 01)
+                        {
+                            Console.ForegroundColor = ConsoleColor.White;
+                            Console.Write("01" + "  ");
+                        }
+                        else {
+                            Console.ForegroundColor = ConsoleColor.DarkGray;
+                            Console.Write("00" + "  ");
+                        }
+                    }
+                    Console.WriteLine();
+                    Console.WriteLine();
+                }
+
+                playBeep();    
+
+                Console.WriteLine("press any key to continue");
+
+                Console.ReadKey();
+                /*   Environment.Exit(0);*/
+            }
+            else
+            {
+                for (int index = 0; index < Path_Row.Length; index++)
+                {
+                    int rowNew = row + Path_Row[index];
+                    int colNew = col + Path_Col[index];
+                    if (CanWeMove(maze, visited, rowNew, colNew))
+                    {
+                        move++;
+                        visited[rowNew, colNew] = move;
+                        drawBoard(rowNew, colNew);
+                        FindPathInMaze(maze, visited, rowNew, colNew, desRow, desCol, move);
+                        move--;
+                        visited[rowNew, colNew] = 0;
+                    }
+                }
+            }
+        }
+
+        bool CanWeMove(int[,] maze, int[,] visited, int rowNew, int colNew)
+        {
+            if ((rowNew >= 0) && (rowNew < 10) && (colNew >= 0) && (colNew < 10) && (maze[rowNew, colNew] == 1) && (visited[rowNew, colNew] == 0))
+            {
+                return true;
+            }
+            return false;
+        }
+
+
+        private void drawBoard(int rowNew, int colNew)
+        {
+            Console.Clear();
+
+            int[,] board = {{1,1,0,0,0,0,0,0,0,1 },
+                            {0,1,1,1,1,1,1,1,1,1 },
+                            {0,1,0,0,0,0,0,0,0,1 },
+                            {0,1,0,0,0,0,0,0,0,1 },
+                            {0,1,0,0,0,0,0,0,0,1 },
+                            {0,1,0,0,0,0,0,0,0,1 },
+                            {0,1,0,0,0,0,0,0,0,1 },
+                            {0,1,0,0,0,0,0,0,0,1 },
+                            {0,1,0,0,0,0,0,0,0,1 },
+                            {0,1,1,1,1,1,1,1,1,1 } };
+
+            if (canRunInitialMove)
+            {
+                initialMove(board);
+                canRunInitialMove = false;
+            }
+
+            board[rowNew, colNew] = 5;
+
+            for (int i = 0; i < board.GetLength(0); i++)
+            {
+                for (int j = 0; j < board.GetLength(1); j++)
+                {
+                    if (board[i, j] == 05)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.Write("05" + "  ");
+
+                    }
+                    else if (board[i, j] == 01) {
+                        Console.ForegroundColor = ConsoleColor.White;
+                        Console.Write("01" + "  ");
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.DarkGray;
+                        Console.Write(board[i, j].ToString("00") + "  ");
+                    }
+                }
+                Console.WriteLine();
+                Console.WriteLine();
+            }
+
+            Thread.Sleep(1000);
+            Console.Clear();
+        }
+
+        private void initialMove(int[,] board)
+        {
+            for (int i = 0; i < board.GetLength(0); i++)
+            {
+                for (int j = 0; j < board.GetLength(1); j++)
+                {
+                    if (board[i, j] == 05)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.Write(board[i, j].ToString("00") + "  ");
+                    }
+                    else if (board[i, j] == 01)
+                    {
+                        if (i == 0 && j == 0)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.Write("05" + "  ");
+                        }
+                        else {
+                            Console.ForegroundColor = ConsoleColor.White;
+                            Console.Write(board[i, j].ToString("00") + "  ");
+                        }
+                      
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.DarkGray;
+                        Console.Write(board[i, j].ToString("00") + "  ");
+                    }
+                }
+                Console.WriteLine();
+                Console.WriteLine();
+            }
+            Thread.Sleep(1000);
+            Console.Clear();
+        }
+
+        private void playBeep()
+        {
+            // Set the Frequency 
+            int frequency1 = 1500;
+            int frequency2 = 2000;
+            int frequency3 = 2500;
+
+            // Set the Duration 
+            int duration = 300;
+
+            int n = 4;
+
+            // Play beep sound n times 
+            for (int i = 0; i < n; i++)
+            {
+                Console.Beep(frequency1, duration);
+            }
+            for (int i = 0; i < n; i++)
+            {
+                Console.Beep(frequency2, duration);
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Beep(frequency3, duration);
+            }
+            Console.Beep(frequency3, 950);
+        }
+    }
+}
---
+++ csharp/theknights tour/V3/KnightTour.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace theknights_tour
+{
+    class KnightTour
+    {
+        int[] pathRow1 = { 2, 1, -1, -2, -2, -1, 1, 2 };
+        int[] pathCol1 = { 1, 2, 2, 1, -1, -2, -2, -1 };
+        bool canRunInitialMove = true;
+
+        public bool FindKnightTour(int[,] visited, int row, int col, int move)
+        {
+            if (move == 64)
+            {
+                for (int i = 0; i < 8; i++)
+                {
+                    for (int j = 0; j < 8; j++)
+                    {
+                        Console.Write(visited[i, j].ToString("00") + "  ");
+                    }
+                    Console.WriteLine();
+                    Console.WriteLine();
+                }
+                return true;
+            }
+            else
+            {
+                for (int index = 0; index < pathRow1.Length; index++)
+                {
+                    int rowNew = row + pathRow1[index];
+                    int colNew = col + pathCol1[index];
+                    if (IfValidMove(visited, rowNew, colNew))
+                    {
+                        move++;
+                        visited[rowNew, colNew] = move;
+                        drawBoard(rowNew, colNew);
+                        if (FindKnightTour(visited, rowNew, colNew, move))
+                        {
+                            return true;
+                        }
+                        move--;
+                        visited[rowNew, colNew] = 0;
+                    }
+                }
+            }
+            return false;
+        }
+
+        private bool IfValidMove(int[,] visited, int rowNew, int colNew)
+        {
+            if ((rowNew >= 0) && (rowNew < 8) && (colNew >= 0) && (colNew < 8) && (visited[rowNew, colNew] == 0))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private void drawBoard(int rowNew, int colNew)
+        {
+            /*  Console.Clear();  */
+
+            int[,] board = {{0,0,0,0,0,0,0,0 },
+                                {0,0,0,0,0,0,0,0 },
+                                {0,0,0,0,0,0,0,0 },
+                                {0,0,0,0,0,0,0,0 },
+                                {0,0,0,0,0,0,0,0 },
+                                {0,0,0,0,0,0,0,0 },
+                                {0,0,0,0,0,0,0,0 },
+                                {0,0,0,0,0,0,0,0 }};
+
+            if (canRunInitialMove)
+            {
+                initialMove();
+                canRunInitialMove = false;
+            }
+
+            board[rowNew, colNew] = 22;
+
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    if (board[i, j] == 22) {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.Write("22" + "  ");
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.DarkGray;
+                        Console.Write("00" + "  ");
+                    }
+                }
+                Console.WriteLine();
+                Console.WriteLine();
+            }
+
+            Thread.Sleep(1000);
+            Console.Clear();
+        }
+
+        private void initialMove()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("22");
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.Write("  00  00  00  00  00  00  00\n\n00  00  00  00  00  00  00  00\n\n00  00  00  00  00  00  00  00\n\n00  00  00  00  00  00  00  00\n\n00  00  00  00  00  00  00  00\n\n00  00  00  00  00  00  00  00\n\n00  00  00  00  00  00  00  00\n\n00  00  00  00  00  00  00  00");
+            Thread.Sleep(1000);
+            Console.Clear();
+        }
+    }
+}
