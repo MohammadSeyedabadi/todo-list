66b8a3f93db6b06b9e020aacb2f19abe7091ac3f
unknown <mhmd.sey.7@gmail.com>
2023-11-06 21:24:10 +0330

    #

---
+++ csharp/updated finall/update maze/maze/maza game/maze/RatInAMaze.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace maze
+{
+    class RatInAMaze
+    {
+        int[] Path_Row = { 0, 0, 1, -1 };
+        int[] Path_Col = { 1, -1, 0, 0 };
+
+        bool canRunInitialMove = true;
+
+
+        public void FindPathInMaze(int[,] maze, int[,] visited, int[,] ratReturns, int row, int col, int desRow, int desCol, int move)
+        {
+            if ((row == desRow) && (col == desCol))
+            {
+                for (int i = 0; i < visited.GetLength(0); i++)
+                {
+                    for (int j = 0; j < visited.GetLength(1); j++)
+                    {
+                        if (visited[i, j] == 01 || visited[i, j] > 01)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.Write(visited[i, j].ToString("00") + "  ");
+                        }
+                        else if (maze[i, j] == 01)
+                        {
+                            Console.ForegroundColor = ConsoleColor.White;
+                            Console.Write("01" + "  ");
+                        }
+                        else
+                        {
+                            Console.ForegroundColor = ConsoleColor.DarkGray;
+                            Console.Write("00" + "  ");
+                        }
+                    }
+                    Console.WriteLine();
+                    Console.WriteLine();
+                }
+
+                playBeep();
+
+                Console.WriteLine("press any key to continue");
+
+                Console.ReadKey();
+                /*   Environment.Exit(0);*/
+                drawBoard(row, col, maze, visited, ratReturns);
+            }
+            else
+            {
+                for (int index = 0; index < Path_Row.Length; index++)
+                {
+                    int rowNew = row + Path_Row[index];
+                    int colNew = col + Path_Col[index];
+                    if (CanWeMove(maze, visited, rowNew, colNew))
+                    {
+                        move++;
+                        visited[rowNew, colNew] = move;
+                        drawBoard(rowNew, colNew, maze, visited, ratReturns);
+                        FindPathInMaze(maze, visited,ratReturns, rowNew, colNew, desRow, desCol, move);
+                        move--;
+                        visited[rowNew, colNew] = 0;
+                        ratReturns[rowNew, colNew] = 1;
+                        drawBoard(row, col, maze, visited, ratReturns);
+                    }
+                }
+            }
+        }
+
+        bool CanWeMove(int[,] maze, int[,] visited, int rowNew, int colNew)
+        {
+            if ((rowNew >= 0) && (rowNew < 10) && (colNew >= 0) && (colNew < 10) && (maze[rowNew, colNew] == 1) && (visited[rowNew, colNew] == 0))
+            {
+                return true;
+            }
+            return false;
+        }
+
+
+        private void drawBoard(int rowNew, int colNew, int[,] maze, int[,] visited, int[,] ratReturns)
+        {
+            Console.Clear();
+
+            if (canRunInitialMove)
+            {
+                initialMove(maze);
+                canRunInitialMove = false;
+            }
+            int temp = maze[rowNew, colNew];
+            maze[rowNew, colNew] = 5;
+
+            for (int i = 0; i < maze.GetLength(0); i++)
+            {
+                for (int j = 0; j < maze.GetLength(1); j++)
+                {
+                    if (maze[i, j] == 05)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.Write("05" + "  ");
+
+                    }
+                    else if (visited[i, j] != 0) {
+                        Console.ForegroundColor = ConsoleColor.Blue;
+                        Console.Write("++" + "  ");
+                    }
+                    else if (ratReturns[i, j] == 1)
+                    {
+                        Console.ForegroundColor = ConsoleColor.DarkMagenta;
+                        Console.Write("//" + "  ");
+                    }
+                    else if (maze[i, j] == 01)
+                    {
+                        Console.ForegroundColor = ConsoleColor.White;
+                        Console.Write("01" + "  ");
+                    }
+
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.DarkGray;
+                        Console.Write(maze[i, j].ToString("00") + "  ");
+                    }
+                }
+                Console.WriteLine();
+                Console.WriteLine();
+            }
+
+            Thread.Sleep(100);
+            Console.Clear();
+            maze[rowNew, colNew] = temp;
+        }
+
+        private void initialMove(int[,] board)
+        {
+            for (int i = 0; i < board.GetLength(0); i++)
+            {
+                for (int j = 0; j < board.GetLength(1); j++)
+                {
+                    if (board[i, j] == 05)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.Write(board[i, j].ToString("00") + "  ");
+                    }
+                    else if (board[i, j] == 01)
+                    {
+                        if (i == 0 && j == 0)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.Write("05" + "  ");
+                        }
+                        else
+                        {
+                            Console.ForegroundColor = ConsoleColor.White;
+                            Console.Write(board[i, j].ToString("00") + "  ");
+                        }
+
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.DarkGray;
+                        Console.Write(board[i, j].ToString("00") + "  ");
+                    }
+                }
+                Console.WriteLine();
+                Console.WriteLine();
+            }
+            Thread.Sleep(100);
+            Console.Clear();
+        }
+
+        private void playBeep()
+        {
+            // Set the Frequency 
+            int frequency1 = 1500;
+            int frequency2 = 2000;
+            int frequency3 = 2500;
+
+            // Set the Duration 
+            int duration = 300;
+
+            int n = 4;
+
+            // Play beep sound n times 
+            for (int i = 0; i < n; i++)
+            {
+                Console.Beep(frequency1, duration);
+            }
+            for (int i = 0; i < n; i++)
+            {
+                Console.Beep(frequency2, duration);
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Beep(frequency3, duration);
+            }
+            Console.Beep(frequency3, 950);
+        }
+    }
+}
---
+++ csharp/updated finall/warnsdorffsAlgorithmKnightsTourProblem/warnsdorffsAlgorithmKnightsTourProblem/GFG.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace warnsdorffsAlgorithmKnightsTourProblem
+{
+    public class GFG
+    {
+
+        public static int N = 8;
+
+        // Move pattern on basis of the change of
+        // x coordinates and y coordinates respectively
+        public int[] cx = new int[] { 1, 1, 2, 2, -1, -1, -2, -2 };
+        public int[] cy = new int[] { 2, -2, 1, -1, 2, -2, 1, -1 };
+
+        // function restricts the knight to remain within
+        // the 8x8 chessboard
+        bool limits(int x, int y)
+        {
+            return ((x >= 0 && y >= 0) && (x < N && y < N));
+        }
+
+        /* Checks whether a square is valid and
+        empty or not */
+        bool isempty(int[] a, int x, int y)
+        {
+            return ((limits(x, y)) && (a[y * N + x] < 0));
+        }
+
+        /* Returns the number of empty squares
+        adjacent to (x, y) */
+        int getDegree(int[] a, int x, int y)
+        {
+            int count = 0;
+            for (int i = 0; i < N; ++i)
+                if ( isempty( a, (x + cx[i]), (y + cy[i]) ) )
+                    count++;
+
+            return count;
+        }
+
+        // Picks next point using Warnsdorff's heuristic.
+        // Returns false if it is not possible to pick
+        // next point.
+        Cell nextMove(int[] a, Cell cell)
+        {
+            int min_deg_idx = -1, c,
+                min_deg = (N + 1), nx, ny;
+
+            // Try all N adjacent of (*x, *y) starting
+            // from a random adjacent. Find the adjacent
+            // with minimum degree.
+            Random random = new Random();
+            int start = random.Next(0, 1000);
+            for (int count = 0; count < N; ++count)
+            {
+                int i = (start + count) % N;
+                nx = cell.x + cx[i];
+                ny = cell.y + cy[i];
+                if ( (isempty(a, nx, ny)) && ( c = getDegree(a, nx, ny) ) < min_deg )
+                {
+                    min_deg_idx = i;
+                    min_deg = c;
+                }
+            }
+
+            // IF we could not find a next cell
+            if (min_deg_idx == -1)
+                return null;
+
+            // Store coordinates of next point
+            nx = cell.x + cx[min_deg_idx];
+            ny = cell.y + cy[min_deg_idx];
+
+            // Mark next move
+            a[ny * N + nx] = a[(cell.y) * N +
+                               (cell.x)] + 1;
+
+            // Update next point
+            cell.x = nx;
+            cell.y = ny;
+
+            return cell;
+        }
+
+        /* displays the chessboard with all the
+        legal knight's moves */
+        void print(int[] a)
+        {
+            for (int i = 0; i < N; ++i)
+            {
+                for (int j = 0; j < N; ++j)
+                    Console.Write( a[j * N + i].ToString("00") + "  " );
+                Console.Write("\n\n");
+            }
+        }
+
+        /* checks its neighbouring squares */
+        /* If the knight ends on a square that is one
+        knight's move from the beginning square,
+        then tour is closed */
+        bool neighbour(int x, int y, int xx, int yy)
+        {
+            for (int i = 0; i < N; ++i)
+                if ( ( (x + cx[i]) == xx) && ( (y + cy[i]) == yy) )
+                    return true;
+
+            return false;
+        }
+
+        /* Generates the legal moves using warnsdorff's
+        heuristics. Returns false if not possible */
+        public bool findClosedTour()
+        {
+            // Filling up the chessboard matrix with -1's
+            int[] a = new int[N * N];
+            for (int i = 0; i < N * N; ++i)
+                a[i] = -1;
+
+            // initial position
+            int sx = 3;
+            int sy = 2;
+
+            // Current points are same as initial points
+            Cell cell = new Cell(sx, sy);
+
+            // Keep picking next points using
+            // Warnsdorff's heuristic
+            Cell ret = null;
+
+            //   drawBoard(a, ret);
+
+            a[cell.y * N + cell.x] = 1; // Mark first move.
+
+           
+           // drawBoard(a, ret);
+
+            // Keep picking next points using
+            // Warnsdorff's heuristic
+           // Cell ret = null;
+
+            for (int i = 0; i < N * N - 1; ++i) {
+                ret = nextMove(a, cell);
+                drawBoard(a, ret);
+                if (ret == null)
+                    return false;
+            }
+
+            // Check if tour is closed (Can end
+            // at starting point)
+            if (!neighbour(ret.x, ret.y, sx, sy)) 
+                return false;
+            
+                
+
+            print(a);
+           
+           // drawBoard(a,ret);
+            return true;
+        }
+
+        private void drawBoard(int[] a, Cell ret)
+        {
+            for (int i = 0; i < N; ++i)
+            {
+                for (int j = 0; j < N; ++j)
+                {
+                    if (a[j * N + i] == -1)
+                    {
+                        Console.ForegroundColor = ConsoleColor.DarkGray;
+                        Console.Write("00  ");
+                        Console.ForegroundColor = ConsoleColor.Green;
+                    }
+                    else if (ret != null)
+                    {
+                        if (i == ret.x && j == ret.y)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.Write("**  ");
+                            Console.ForegroundColor = ConsoleColor.Green;
+                        }
+                        else
+                        {
+                            Console.Write(a[j * N + i].ToString("00") + "  ");
+                        }
+                    }
+
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.Write("**  ");
+                        Console.ForegroundColor = ConsoleColor.Green;
+                    }
+                }
+
+                Console.Write("\n\n");
+            }
+            Thread.Sleep(100);
+            for (int i = 0; i < a.Length; i++) {
+                if (a[i] == 64)
+                {
+                    Console.ReadKey();
+                }
+            }
+                Console.Clear();
+        }
+    }
+}
